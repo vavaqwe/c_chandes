f1cb253bff0ea4180c3b8e7ce1c9cdfb5bd1d670
vaval <vaval@DESKTOP-37SNVMR>
2023-04-22 21:02:29 +0300

    Добавьте файлы проекта.

---
+++ curvva/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace curvva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTogglePanel = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelContainer.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 362);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.removeButton);
+            this.panelContainer.Controls.Add(this.buttonAdd);
+            this.panelContainer.Controls.Add(this.textBox1);
+            this.panelContainer.Controls.Add(this.button1);
+            this.panelContainer.Controls.Add(this.label2);
+            this.panelContainer.Location = new System.Drawing.Point(459, 44);
+            this.panelContainer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(275, 366);
+            this.panelContainer.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Путь к игре";
+            // 
+            // btnTogglePanel
+            // 
+            this.btnTogglePanel.Location = new System.Drawing.Point(359, 417);
+            this.btnTogglePanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTogglePanel.Name = "btnTogglePanel";
+            this.btnTogglePanel.Size = new System.Drawing.Size(75, 23);
+            this.btnTogglePanel.TabIndex = 6;
+            this.btnTogglePanel.Text = "button2";
+            this.btnTogglePanel.UseVisualStyleBackColor = true;
+            this.btnTogglePanel.Click += new System.EventHandler(this.BtnTogglePanel_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(143, 32);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 11F);
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(142, 28);
+            this.закрытьToolStripMenuItem.Text = "закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.ЗакрытьToolStripMenuItem_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(231, 102);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(25, 24);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(200, 102);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(25, 24);
+            this.removeButton.TabIndex = 4;
+            this.removeButton.Text = "-";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Visible = false;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 452);
+            this.Controls.Add(this.btnTogglePanel);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTogglePanel;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
+
+
---
+++ curvva/Form1.cs
@@ -0,0 +1,312 @@
+﻿using NAudio.CoreAudioApi;
+using Microsoft.Win32;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing;
+
+namespace curvva
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private IPAddress Set_ip()
+        {
+            string hostName = Dns.GetHostName(); // Получить имя хоста
+            IPAddress[] ipAddress = Dns.GetHostAddresses(hostName); // Получить все IP-адреса для этого хоста
+            IPAddress ipAdd = null;
+            foreach (IPAddress ipAddres in ipAddress)
+            {
+                if (ipAddres.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) // Выбрать только IPv4 адреса
+                {
+                    string ip = ipAddres.ToString();
+                    ipAdd = ipAddres;
+                }
+            }
+            return ipAdd;
+        }
+        string Set_1p()
+        {
+            string hostName = Dns.GetHostName(); // Получить имя хоста
+            IPAddress[] ipAddress = Dns.GetHostAddresses(hostName); // Получить все IP-адреса для этого хоста
+            string ip = null;
+            foreach (IPAddress ipAddres in ipAddress)
+            {
+                if (ipAddres.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) // Выбрать только IPv4 адреса
+                {
+                    ip = ipAddres.ToString();
+                    IPAddress ipAdd = ipAddres;
+                }
+            }
+            return ip;
+        }
+        private async void Server()
+        {
+            try
+            {
+                IPAddress ipAdd = Set_ip();
+                // Создание серверного сокета
+                TcpListener listener = new TcpListener(ipAdd, 12345);
+                listener.Start();
+                string user = Environment.UserName.ToString();
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+
+                    try
+                    {
+                        NetworkStream stream = client.GetStream();
+                        byte[] buffer = new byte[1024];
+
+                        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        int req = message[0] - '0';
+
+                        if (req == 0)
+                        {
+                            Process cmd = new Process();
+                            cmd.StartInfo.FileName = "cmd.exe";
+                            cmd.StartInfo.RedirectStandardInput = true;
+                            cmd.StartInfo.UseShellExecute = false;
+                            cmd.Start();
+                            cmd.StandardInput.WriteLine("shutdown /s /t 0");
+                            cmd.WaitForExit();
+                        }
+                        if (req == 1)
+                        {
+                            string path = $@"C:\Users\{user}\AppData\Local\Discord\Update.exe";
+                            string arguments = "--processStart Discord.exe";
+
+                            Process process = new Process();
+                            process.StartInfo.FileName = path;
+                            process.StartInfo.Arguments = arguments;
+                            process.Start();
+
+
+                        }
+                        if (req == 2)
+                        {
+                            Process.Start("https://google.com");
+                        }
+                        if (req == 3)
+                        {
+                            Process.Start("https://www.youtube.com/watch?v=0-C0lCPFTj8");
+                        }
+                        if (req == 4)
+                        {
+                            Process cmd = new Process();
+                            cmd.StartInfo.FileName = "cmd.exe";
+                            cmd.StartInfo.RedirectStandardInput = true;
+                            cmd.StartInfo.UseShellExecute = false;
+                            cmd.Start();
+                            cmd.StandardInput.WriteLine("shutdown /r /t 0");
+                            cmd.WaitForExit();
+                        }
+                        if (req == 5)
+                        {
+                            string newValue = ConfigurationManager.AppSettings["game"];
+                            Process.Start(newValue);
+                        }
+                        if (req == 6)
+                        {
+                            string cutURI = HttpUtility.UrlDecode(message, Encoding.UTF8);
+                            cutURI = cutURI.Remove(0, 1);
+                            if (cutURI[4].Equals("https") && cutURI[3].Equals("http"))
+                            {
+                                Process.Start(cutURI);
+                            }
+                            else
+                            {
+                                string encodedQuery = HttpUtility.UrlEncode(cutURI);
+                                Process.Start("https://www.google.com/search?q=" + encodedQuery);
+                            }
+
+                        }
+                        if (req == 7)
+                        {
+                            string cutURI = HttpUtility.UrlDecode(message, Encoding.UTF8);
+                            cutURI = cutURI.Remove(0, 1);
+
+                            string encodedQuery = HttpUtility.UrlEncode(cutURI);
+                            Process.Start("https://www.youtube.com/results?search_query=" + encodedQuery);
+                        }
+                        if (req == 8)
+                        {
+                            string cutURI = message.Remove(0, 1);
+                            float volume = float.Parse(cutURI, CultureInfo.InvariantCulture.NumberFormat);
+                            volume /= 100;
+                            MMDevice defaultPlaybackDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+                            // устанавливаем громкость 
+                            defaultPlaybackDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+                        }
+                        client.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "curva";
+            notifyIcon1.BalloonTipText = "приложение закрыть а ты хуесос можешь кнопочки на телефоне нажимать";
+            notifyIcon1.Text = "curva";
+
+            panelContainer.Width = 0;
+            panelContainer.Visible = false;
+            btnTogglePanel.Text = ">>";
+
+            string ip = Set_1p();
+            string myVariable = ConfigurationManager.AppSettings["start_up"];
+            if (myVariable.Equals('1'))
+            {
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "curva.exe");
+                RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rkApp.SetValue("MyApp", filePath);
+
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings["start_up"].Value = "0";
+                config.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            label1.Text = ip;
+            QRCoder.QRCodeGenerator QR = new QRCoder.QRCodeGenerator();
+            var MyData = QR.CreateQrCode(ip, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(MyData);
+            pictureBox1.Image = code.GetGraphic(50);
+            Server();
+        }
+
+        private void BtnTogglePanel_Click(object sender, EventArgs e)
+        {
+            if (panelContainer.Width == 0)
+            {
+                panelContainer.Visible = true;
+                this.Width = 600;
+                panelContainer.Width = 200; // Ширина панели.
+                panelContainer.BringToFront();
+                btnTogglePanel.Text = "<<"; // Текст кнопки для скрытия панели.
+            }
+            else
+            {
+                // Если ширина панели больше 0, значит панель открыта, и мы ее скрываем.
+                panelContainer.Visible = false;
+                this.Width = 350;
+                panelContainer.Width = 0; // Ширина панели.
+                btnTogglePanel.Text = ">>"; // Текст кнопки для открытия панели.
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Получаем текущее значение переменной из app.config
+            string currentValue = ConfigurationManager.AppSettings["game"];
+
+            // Изменяем значение переменной на "new value"
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["game"].Value = textBox1.Text;
+            config.Save(ConfigurationSaveMode.Modified);
+
+            // После сохранения изменений, обновляем значения в ConfigurationManager
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void ЗакрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+        private Button myButton;
+        private TextBox myTextBox;
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // Создайте новую кнопку и текстовое поле
+            myButton = new Button();
+            myTextBox = new TextBox();
+
+            // Настройте свойства созданных элементов
+            myButton.Text = "Новая кнопка";
+            myButton.Size = button1.Size;
+            myButton.Location = new Point(67, 140);
+
+            myTextBox.Size = textBox1.Size;
+            myTextBox.Location = new Point(21, 120);
+
+            removeButton.Visible = true;
+            panelContainer.Controls.Add(myButton);
+            panelContainer.Controls.Add(myTextBox);
+            myButton.Click += new EventHandler(myButton_Click);
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (myButton != null)
+            {
+                panelContainer.Controls.Remove(myButton);
+                myButton.Dispose();
+                myButton = null;
+            }
+
+            if (myTextBox != null)
+            {
+                panelContainer.Controls.Remove(myTextBox);
+                myTextBox.Dispose();
+                myTextBox = null;
+            }
+            removeButton.Visible = false;
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Кнопка была нажата!");
+        }
+
+    }
+}
+
+
+
